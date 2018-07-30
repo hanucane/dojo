623b2af03e7cbf066c92e2e06b936896ff536b4b
Soto <soto.ericf@gmail.com>
2018-07-30 14:48:41 -0700

    Beginning of C#/.net

---
+++ Bootcamp/bootcamp-c#_net/Fundamentals/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Randomness();
+        }
+        public static void NumLoop()
+        {
+            int i=1;
+            while (i<=255){
+                Console.WriteLine(i);
+                i += 1;
+            }
+        }
+        public static void Divisible()
+        {
+            int i=1;
+            while (i<=100){
+                if (i%3 == 0 || i%5 == 0){
+                    if(i%3 == 0 && i%5 == 0){}
+                    else {
+                        Console.WriteLine(i);
+                    }
+                }
+                i+=1;
+            }
+        }
+        public static void DivisibleStr()
+        {
+            int i=1;
+            while (i<=100){
+                if (i%3 == 0 || i%5 == 0){
+                    if(i%3 == 0 && i%5 == 0){
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if(i%3 == 0) {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if(i%5 == 0) {
+                        Console.WriteLine("Buzz");
+                    }
+                }
+                i+=1;
+            }
+        }
+        public static void Randomness()
+        {
+            for(int val = 0; val < 10; val++)
+            {
+                Random rand  = new Random();
+                int random = rand.Next(1,20);
+                if (random%3 == 0 || random%5 == 0){
+                    if(random%3 == 0 && random%5 == 0){
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if(random%3 == 0) {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if(random%5 == 0) {
+                        Console.WriteLine("Buzz");
+                    }
+                }
+            }
+        }
+    }
+}
