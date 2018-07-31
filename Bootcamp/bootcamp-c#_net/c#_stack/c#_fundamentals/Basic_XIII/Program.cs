9eb29ee9dcf8812e6cb85bef3fd12259b85e5920
Soto <soto.ericf@gmail.com>
2018-07-30 20:13:09 -0700

    C# folder structure

---
+++ Bootcamp/bootcamp-c#_net/c#_stack/c#_fundamentals/Basic_XIII/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basic_XIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] arr = {1,-3,4,-6,8,10};
+            NumberToString(arr);
+        }
+
+        public static void Print1to255()
+        {
+            int i = 1;
+            while (i<=255){
+                Console.WriteLine(i);
+                i += 1;
+            }
+        }
+        public static void PrintOdd1to255()
+        {
+            int i = 1;
+            while (i<=255){
+                if (i%2!=0){
+                    Console.WriteLine(i);
+                }
+                i += 1;
+            }
+        }
+        public static void PrintSum0to255()
+        {
+            int i = 0;
+            double sum = 0;
+            while (i<=255){
+                sum += i;
+                Console.WriteLine("New Number: "+i+" Sum: "+sum);
+                i += 1;
+            }
+        }
+        public static void IterateArray(int[] arr)
+        {
+            foreach (object x in arr){
+                Console.WriteLine(x);
+            }
+        }
+        public static void Max(int[] arr)
+        {
+            int max = arr[0];
+            foreach (int x in arr){
+                if (x > max){
+                    max = x;
+                }
+            }
+            Console.WriteLine(max);
+        }
+        public static void Average(int[] arr)
+        {
+            float sum = 0;
+            for (int i=0; i<arr.Length; i++){
+                sum += arr[i];
+            }
+            float avg = sum/arr.Length;
+            Console.WriteLine(avg);
+        }
+        public static void ArrayOdd()
+        {
+            List<int> odd = new List<int>();
+            int i = 1;
+            while (i<=255){
+                if (i%2!=0){
+                    odd.Add(i);
+                }
+                i += 1;
+            }
+            for (int x = 0; x < odd.Count; x++){
+                Console.WriteLine(odd[x]);
+            }
+        }
+        public static void GreaterThanY(int[] arr, int y)
+        {
+            int total = 0;
+            for (int i = 0; i < arr.Length; i++){
+                if (arr[i] > y){
+                    total += 1;
+                }
+            }
+            Console.WriteLine(total);
+        }
+        public static void SquareValues(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++){
+                arr[i] *= arr[i];
+            }
+            foreach (int x in arr){
+                Console.WriteLine(x);
+            }
+        }
+        public static void EliminateNegatives(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++){
+                if (arr[i] < 0){
+                    arr[i] = 0;
+                }
+            }
+            foreach (int x in arr){
+                Console.WriteLine(x);
+            }
+        }
+        public static void MaxMinAvg(int[] arr)
+        {
+            int max = arr[0];
+            int min = arr[0];
+            float sum = 0;
+            for (int x=1; x<arr.Length; x++){
+                if (arr[x] > max){
+                    max = arr[x];
+                }
+                else if (arr[x] < min){
+                    min = arr[x];
+                }
+                sum += arr[x];
+            }
+            float avg = sum / arr.Length;
+            Console.WriteLine("Max: "+max+" Min: "+min+" Average: "+avg);
+        }
+        public static void ShiftValues(int[] arr)
+        {
+            for (int i=0; i<arr.Length; i++){
+                if (i == arr.Length-1){
+                    arr[i] = 0;
+                }
+                else {
+                    arr[i] = arr[i+1];
+                }
+            }
+            foreach (int x in arr){
+                Console.WriteLine(x);
+            }
+        }
+        public static void NumberToString(object[] arr)
+        {
+            for (int i=0; i<arr.Length; i++){
+                if ((int)arr[i] < 0){
+                    arr[i] = "Dojo";
+                }
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ Bootcamp/bootcamp-c#_net/c#_stack/c#_fundamentals/Puzzles/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Names();
+        }
+        public static List<int> RandomArray()
+        {
+            List<int> values = new List<int>();
+            Random rand = new Random();
+            int i = 1;
+            while (i<=10){
+                values.Add(rand.Next(5,25));
+                i += 1;
+            }
+            int max = values[0];
+            int min = values[0];
+            float sum = 0;
+            for (int x=1; x<values.Count; x++){
+                if (values[x] > max){
+                    max = values[x];
+                }
+                else if (values[x] < min){
+                    min = values[x];
+                }
+                sum += values[x];
+            }
+            Console.WriteLine("Max: "+max+" Min: "+min+" Sum: "+sum);
+            return values;
+        } 
+        public static string CoinFlip()
+        {
+            Random rand = new Random();
+            string flip = "";
+            int toss = rand.Next(2);
+            if (toss == 1){
+                flip = "Heads";
+            }
+            else {
+                flip = "Tails";
+            }
+            Console.WriteLine(flip);
+            return flip;
+        }
+        public static List<string> Names()
+        {
+            string[] arr = {"Todd","Tiffany","Charlie","Geneva","Sydney"};
+            for (int i = 0; i < arr.Length/2; i++){
+                string temp = arr[i];
+                arr[i] = arr[arr.Length-(i+1)];
+                arr[arr.Length-(i+1)] = temp;
+            }
+            List<string> arrnew = new List<string>();
+            for (int i = 0; i < arr.Length; i++){
+                if (arr[i].Length > 5){
+                    arrnew.Add(arr[i]);
+                }
+            }
+            foreach (string x in arrnew){
+                Console.WriteLine(x);
+            }
+            return arrnew;
+        }
+    }
+}
