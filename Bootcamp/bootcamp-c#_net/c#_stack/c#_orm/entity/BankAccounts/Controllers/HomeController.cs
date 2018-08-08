f83082674b7e733136e1e65a22e6bf143df6bda2
Soto <soto.ericf@gmail.com>
2018-08-08 03:21:29 -0700

    C# entity Bank Account Start

---
+++ Bootcamp/bootcamp-c#_net/c#_stack/c#_orm/entity/BankAccounts/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BankAccounts.Models;
+
+namespace BankAccounts.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Users _context;
+        public HomeController(Users context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Process(User NewUser, string confirm_password)
+        {
+            if(ModelState.IsValid)
+            {
+                if (NewUser.password == confirm_password){
+                    PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                    NewUser.password = Hasher.HashPassword(NewUser, NewUser.password);
+                    _context.Add(NewUser);
+                    _context.SaveChanges();
+                    HttpContext.Session.SetString("login", NewUser.email);
+
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("password", "Passwords must match.");
+            }
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bootcamp/bootcamp-c#_net/c#_stack/c#_orm/entity/BankAccounts/Models/User.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankAccounts.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage="First name must be at least 2 characters")]
+        public string first_name { get; set; }
+        [Required]
+        [MinLength(2, ErrorMessage="Last name must be at least 2 characters")]
+        public string last_name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage="Please enter a valid e-mail")]
+        public string email { get; set; }
+        [Required]
+        [MinLength(8, ErrorMessage="Password must be at least 8 characters")]
+        public string password { get; set; }
+        
+    }
+}
