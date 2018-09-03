2ecaf26ee9496c56de83f97ef4f14dc8e965eab2
Soto <soto.ericf@gmail.com>
2018-09-03 00:26:40 -0700

    Start of MEAN

--- Bootcamp/bootcamp-algorithms/morning-algo/8-20-18.js
+++ Bootcamp/bootcamp-algorithms/morning-algo/8-20-18.js
@@ -9,6 +9,7 @@ function BST(){
     this.add(val) = function add(val){
         if (this.root == null){
             this.root= new Node(val);
+            return this.root;
         }
         else {
             runner = this.root;
@@ -16,17 +17,18 @@ function BST(){
                 if(val < runner.val){
                     if(runner.left == null){
                         runner.left = new Node(val);
+                        return this.root;
                     }
                     runner = runner.left;
                 }
                 if(val >= runner.val){
                     if(runner.right == null){
                         runner.right = new Node(val);
+                        return this.root;
                     }
                     runner = runner.right;
                 }
             }
         }
-        return this.root;
     }
 }
---
+++ Bootcamp/bootcamp-mean/mean_algorithms/mean-intro.js
@@ -0,0 +1,44 @@
+function maxMinAvg(arr){
+    var max = arr[0]
+    var min = arr[0]
+    var sum = arr[0]
+    for(var x = 1; x < arr.length; x++){
+        if(arr[x] > max){
+            max = arr[x]
+        }
+        else if (arr[x] < min){
+            min = arr[x]
+        }
+        sum += arr[x]
+    }
+    var avg = sum / arr.length
+
+    return console.log("The minimum is "+min+", the maximum is "+max+", and the average is "+avg+".")
+}
+
+function fizzbuzz(int){
+    if (typeof int === 'string'){
+        return console.log("Must enter a positive integer")
+    }
+    for (var x = 1; x <= int; x++)
+    {
+        if (x % 3 == 0 && x % 5 == 0){
+            console.log("FizzBuzz")
+        }
+        else if (x % 3 == 0){
+            console.log("Fizz")
+        }
+        else if (x % 5 == 0){
+            console.log("Buzz")
+        }
+        else{
+            console.log(x)
+        }
+    }
+}
+
+function bracesValid(str){
+    for (var x = 0; x < str.length; x++){
+        
+    }
+}
