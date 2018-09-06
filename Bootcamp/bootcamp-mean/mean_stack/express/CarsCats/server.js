a37756b6051f3ef9ed29c1c74df3d547c50acbf4
Soto <soto.ericf@gmail.com>
2018-09-05 17:28:29 -0700

    cars and cats details

--- Bootcamp/bootcamp-mean/mean_stack/express/CarsCats/server.js
+++ Bootcamp/bootcamp-mean/mean_stack/express/CarsCats/server.js
@@ -12,18 +12,28 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 // use app's get method and pass it the base route '/' and a callback
 app.get('/cars', function(request, response) {
-    // just for fun, take a look at the request and response objects
-   console.log("The request object", request);
-   console.log("The response object", response);
-   // use the response object's .send() method to respond with an h1
-   response.render('cars');
-})
+        response.render('cars');
+    })
 app.get('/cats', function(request, response) {
-       response.render('cats');
+        response.render('cats');
     })
 app.get('/cars/new', function(request, response) {
         response.render('form');
-     })
+    })
+app.get("/details/:catId", function (request, response){
+        let catId = request.params.catId;
+        var cat_array = [
+            {name: "Sgt. Meowinstein", favfood: "grass", age: "1", sleep: ["under car", "in a well"]}, 
+            {name: "Undead Kitty", favfood: "human flesh", age: "2000", sleep: ["under the bed", "in a sunbeam"]}
+        ];
+        for (let i=0; i<cat_array.length; i++)
+        {
+            if (i == catId){
+                var obj = cat_array[i];
+            }
+        }
+        response.render('details', {cats: obj});
+    })
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 app.listen(8000, function() {
   console.log("listening on port 8000");
